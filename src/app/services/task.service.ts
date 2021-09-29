cff27aa426c3745ea7d4800627eaabac50991b2d
kmadhav907 <kmadhav907@gmail.com>
2021-09-29 17:38:36 +0530

    Simple tasks done

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../Task';
+import { TASKS } from '../mock-tasks';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  constructor() {}
+}
